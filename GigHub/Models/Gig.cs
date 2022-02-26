c2587f832acfeb43c9c6432922d29d6c78005b99
NourNafea <nour68.nafea@gmail.com>
2022-02-27 00:01:15 +0200

    Disablle nullable

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using GigHub.Migrations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -7,8 +8,16 @@ namespace GigHub.Models;
 public class Gig
 {
     public int Id { get; set; }
+    
+    [Required]
     public IdentityUser Artist  { get; set; }
+    
     public DateTime DateTime { get; set; }
+    
+    [Required]
+    [StringLength(255)]
     public string Venue { get; set; }
+    
+    [Required]
     public Genre Genre { get; set; }
 }
