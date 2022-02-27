32bfa8d5e4f51e022b1cc882c547d5f1bca07fb0
NourNafea <nour68.nafea@gmail.com>
2022-02-27 11:21:17 +0200

    Add gig controller

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,8 @@
+namespace GigHub.ViewModels;
+
+public class GigFormViewModel
+{
+    public string Venue { get; set; }
+    public string Date { get; set; }
+    public string Time { get; set; }
+}
